f1d385b5afb5c0105e277cfa04e40e40265e9873
Eden Leung <liangjinbiao@live.com>
2020-01-07 05:52:07 +0000

    Apply fixes from StyleCI

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'sso'         => true,
-    'ssoCacheKey' => 'jwt-auth-user',
-    'ssoKey'      => 'uid',
-    'signerKey'   => '',
-    'notBefore'   => 0,
-    'expiresAt'   => 3600,
-    'signer'      => 'Lcobucci\JWT\Signer\Hmac\Sha256',
-    'injectUser'  => false,
-    'userModel'   => '',
-    'hasLogged'   => 50401,
-    'tokenAlready' => 50402
+    'sso'          => true,
+    'ssoCacheKey'  => 'jwt-auth-user',
+    'ssoKey'       => 'uid',
+    'signerKey'    => '',
+    'notBefore'    => 0,
+    'expiresAt'    => 3600,
+    'signer'       => 'Lcobucci\JWT\Signer\Hmac\Sha256',
+    'injectUser'   => false,
+    'userModel'    => '',
+    'hasLogged'    => 50401,
+    'tokenAlready' => 50402,
 ];
